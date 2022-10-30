4bda49bce842b35e2358c7bb57d2b678c521a598
MRI6HFB <MRI6HFB@virginia.edu>
2022-10-30 16:16:50 -0400

    Changed cluster settings

--- Cluster.h
+++ Cluster.h
@@ -14,9 +14,9 @@
 #include <string>
 
 // Max distance in pixels squared
-#define CLUSTER_MIN_POINTS 5
-#define CLUSTER_KING_MIN_POINTS 5
-#define CLUSTER_MAX_DISTANCE_SQUARE 400
+#define CLUSTER_MIN_POINTS 50
+#define CLUSTER_KING_MIN_POINTS 50
+#define CLUSTER_MAX_DISTANCE_SQUARE 200
 
 enum PointType {RED, BLUE, YELLOW};
 
