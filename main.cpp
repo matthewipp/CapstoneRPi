6cd0fae48160bd68bcd179e33f80c9d1a6b06215
MRI6HFB <MRI6HFB@virginia.edu>
2022-12-09 00:33:17 -0500

    Fixed capture flag print again

--- main.cpp
+++ main.cpp
@@ -251,7 +251,7 @@ int runCheckers(int argc, char** argv) {
                         std::cout << "Sent Wait Home\n";
                     }
                     if(FLAG_SEND_CAN_CAPTURE & fsm.currentOutput[2]) {
-                        std::cout << "Sent Can Capture Flag";
+                        std::cout << "Sent Can Capture Flag\n";
                     }
                     uart.sendData(fsm.currentOutput, fsm.outputLength);
                     fsm.lastDataSent = std::chrono::system_clock::now();
