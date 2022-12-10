a846368bb609e250f28b5eeb6c4cc9ae2e49d1fa
MRI6HFB <MRI6HFB@virginia.edu>
2022-12-10 12:07:42 -0500

    Added CheckersPiece comparison function

--- ImageState.h
+++ ImageState.h
@@ -33,6 +33,12 @@ struct CheckersPiece {
     bool isKing;
 };
 
+bool operator==(const CheckersPiece& lhs, CheckersPiece& rhs) {
+    bool coordsMatch = (lhs.imageX == rhs.imageX) && (lhs.imageY == rhs.imageY);
+    bool typeMatch = (lhs.isBlue == rhs.isBlue) && (lhs.isKing == rhs.isKing);
+    return coordsMatch && typeMatch;
+}
+
 struct IncorrectSquare {
     int x;
     int y;
